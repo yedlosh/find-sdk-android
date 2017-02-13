ab27b87c09604d6b0f05f6e407d841fecb956233
Matthew Clarke <matthew.clarke@orionhealth.com>
2017-02-13 16:45:27 +1300

    Disabled swiping between tabs in accordance with material spec

---
+++ sample/src/main/java/com/orchestral/findsdksample/NonSwipeableViewPager.java
@@ -0,0 +1,34 @@
+package com.orchestral.findsdksample;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Copyright © 2017. Orion Health. All rights reserved.
+ */
+
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+}
